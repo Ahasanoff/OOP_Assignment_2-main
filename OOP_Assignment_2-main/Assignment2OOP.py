065d24ffded92feb4d7af02948f3c05298184933
Ali Hasanoff <lhsnff@gmail.com>
2023-11-14 14:40:31 +1030

    Assignment 2 commit 1. Added class Alchemist

--- OOP_Assignment_2-main/Assignment2OOP.py
+++ OOP_Assignment_2-main/Assignment2OOP.py
@@ -1,2 +1,27 @@
-print("3rd try. first commit")
-print("second commit - confirm if it works")
+class Alchemist:
+    def __init__(self):
+        self.attack = 0
+        self.strength = 0
+        self.defense = 0
+        self.magic = 0
+        self.ranged = 0
+        self.necromancy = 0
+        self.laboratory = Laboratory
+        self.recipes = {
+            'Super Attack': ('Irit', 'Eye of Newt'),
+            'Super Strength': ('Kwuarm', 'Limpwurt Root'),
+            'Super Defence': ('Cadantine', 'White Berries'),
+            'Super Magic': ('Lantadyme', 'Potato Cactus'),
+            'Super Ranging': ('Dwarf Weed', 'Wine of Zamorak'),
+            'Super Necromancy': ('Arbuck', 'Blood of Orcus'),
+            'Extreme Attack': ('Avantoe', 'Super Attack'),
+            'Extreme Strength': ('Dwarf Weed', 'Super Strength'),
+            'Extreme Defence': ('Lantadyme', 'Super Defence'),
+            'Extreme Magic': ('Ground Mud Rune', 'Super Magic'),
+            'Extreme Ranging': ('Grenwall Spike', 'Super Ranging'),
+            'Extreme Necromancy': ('Ground Miasma Rune', 'Super Necromancy'),
+        }
+    
+
+class Laboratory:
+    pass
